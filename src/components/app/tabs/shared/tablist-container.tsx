77acf5e8a5bea8207b598b579c03b100c89d23e6
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2026-04-30 09:41:45 +0200

    refactor: ajouter le composant TabContentContainer pour une meilleure structure des onglets

--- src/components/app/tabs/shared/tablist-container.tsx
+++ src/components/app/tabs/shared/tablist-container.tsx
@@ -14,3 +14,7 @@ export const TablistContainer: React.FC<React.ComponentProps<"div">> = ({ classN
 export const SubTablistContainer: React.FC<React.ComponentProps<"div">> = ({ className, ...props }) => {
   return <div className={cn("shrink-0 overflow-x-auto overflow-y-hidden border-b border-accent-foreground bg-primary/60 text-primary-foreground", className)} {...props} />;
 };
+
+export const TabContentContainer: React.FC<React.ComponentProps<"div">> = ({ className, ...props }) => {
+  return <div className={cn("px-32 py-8", className)} {...props} />;
+};
